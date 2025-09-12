f27e2bb0f79a2f8b27f22154e5f4865a81c2e702
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2025-09-12 21:52:24 +0530

    updates

--- check_outlet.js
+++
@@ -1,7 +0,0 @@
-const db = require('./backend/config/db');
-
-const outlets = db.prepare('SELECT outletid, outlet_name FROM mst_outlets LIMIT 10').all();
-console.log('Outlets:', outlets);
-
-const kotSettings = db.prepare('SELECT outletid FROM mstkot_print_settings LIMIT 10').all();
-console.log('KOT Settings:', kotSettings);
--- check_tables.js
+++
@@ -1,7 +0,0 @@
-const db = require('./backend/config/db');
-
-const tables = db.prepare('SELECT tableid, table_name, outletid FROM msttablemanagement WHERE outletid = 19').all();
-console.log('Tables with outletid 19:', tables);
-
-const allTables = db.prepare('SELECT tableid, table_name, outletid FROM msttablemanagement LIMIT 10').all();
-console.log('First 10 tables:', allTables);
