fed73cd4cd028e5ba9e062fde652f45f047654e4
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2025-11-22 14:32:27 +0530

    updates

--- src/views/apps/Transaction/Orders.tsx
+++ src/views/apps/Transaction/Orders.tsx
@@ -2948,8 +2948,16 @@ try {
   }
   return (
     <div className="container-fluid p-0 m-0 fade-in" style={{ height: '100vh' }}>
-      {/* Hidden KOT Preview for Printing */}
-     <div id="kot-preview" style={{ display: 'none' }}>
+      {/* Hidden KOT Preview for Printing */}<div
+  id="kot-preview"
+  style={{
+    position: 'absolute',
+    left: '-9999px',
+    top: '0',
+    visibility: 'hidden',
+    width: '302px'
+  }}
+>
   <div style={{
     width: '80mm',
     margin: '0 auto',
