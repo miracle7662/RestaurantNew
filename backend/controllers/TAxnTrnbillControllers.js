331729770581e64085220cdce9f7ed6d4ecf9fba
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2026-01-19 11:38:05 +0530

    updates

--- backend/controllers/TAxnTrnbillControllers.js
+++ backend/controllers/TAxnTrnbillControllers.js
@@ -3810,7 +3810,7 @@ exports.getGlobalKOTNumber = async (req, res) => {
         `
       SELECT MAX(KOTNo) as maxKOT
       FROM TAxnTrnbilldetails
-      WHERE outletid = ?
+      WHERE outletid = ? AND date(KOTUsedDate) = date('now')
     `,
       )
       .get(Number(outletid))
