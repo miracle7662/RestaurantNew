764395c1a87e530a8470cd84ce18f0f2bef7e78c
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2026-02-07 15:50:52 +0530

    updates

--- src/common/helpers/httpClient.ts
+++ src/common/helpers/httpClient.ts
@@ -17,12 +17,12 @@ function HttpClient() {
     )
 
   const _httpClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
-    timeout: 6000,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
+  timeout: 6000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 
   _httpClient.interceptors.response.use((response) => {
