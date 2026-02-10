60e84f84c66228a395be0705fed2ab59eb5673e9
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2026-02-10 23:15:02 +0530

    updates

---
+++ backend/routes/msttableDepartmentRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const msttableDepartmentController = require('../controllers/msttableDepartmentController');
+
+// Get all departments
+router.get('/', msttableDepartmentController.getAllDepartments);
+
+// Get a single department by ID
+router.get('/', msttableDepartmentController.getDepartmentById);
+
+// Create a new department
+router.post('/', msttableDepartmentController.createDepartment);
+
+// Update a department
+router.put('/:id', msttableDepartmentController.updateDepartment);
+
+// Delete a department
+router.delete('/:id',  msttableDepartmentController.deleteDepartment);
+
+module.exports = router;
