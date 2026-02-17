1377214efc6f919c21a95333e1123a35a1aa3148
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2026-02-17 17:21:49 +0530

    updates

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,20 +1,17 @@
 //import { ThemeSettings, useThemeContext } from '@/common'
 import {
-  Activity,
-  Applications,
+ 
   DarkLight,
   Helpdesk,
   Languages,
   Notifications,
   Profile,
-  Search,
+ 
   // useThemeCustomizer,
 } from '@/components'
 //import Logo from '@/components/Common/Logo'
-import { useViewport } from '@/hooks'
 import { useState } from 'react'
 //import { Link } from 'react-router-dom'
-import MegaMenu from './MegaMenu'
 import { useAuthContext } from '@/common/context/useAuthContext'
 
 type HeaderProps = {
@@ -23,7 +20,7 @@ type HeaderProps = {
 }
 
 const Header = ({ toggleMenu, navOpen }: HeaderProps) => {
-  const { width } = useViewport()
+ 
   // const { sidenavType } = useThemeCustomizer()
   // const { updateSidebar } = useThemeContext()
   const [megaMenuOpen, setMegaMenuOpen] = useState(false)
@@ -63,26 +60,7 @@ const Header = ({ toggleMenu, navOpen }: HeaderProps) => {
     hideBackdrop()
   }
 
-  const handleLeftMenuCallBack = () => {
-    if (width < 768) {
-    //   if (sidenavType === 'iconbar') {
-    //     showLeftSideBarBackdrop()
-    //     document.getElementsByTagName('html')[0].classList.add('sidebar-enable')
-    //   } else {
-    //     updateSidebar({ size: ThemeSettings.sidebar.size.iconbar })
-    //   }
-    // } else if (sidenavType === 'iconbar') {
-    //   updateSidebar({ size: ThemeSettings.sidebar.size.default })
-    // } else if (sidenavType === 'full') {
-    //   showLeftSideBarBackdrop()
-    //   document.getElementsByTagName('html')[0].classList.add('sidebar-enable')
-    // } else if (sidenavType === 'fullscreen') {
-    //   updateSidebar({ size: ThemeSettings.sidebar.size.default })
-    //   document.getElementsByTagName('html')[0].classList.add('sidebar-enable')
-    // } else {
-    //   updateSidebar({ size: ThemeSettings.sidebar.size.iconbar })
-    }
-  }
+  
 
   // function showLeftSideBarBackdrop() {
   //   const backdrop = document.createElement('div')
@@ -110,42 +88,25 @@ const Header = ({ toggleMenu, navOpen }: HeaderProps) => {
         <div className="header-inner px-2 px-md-3">
           {/* header-left */}
           <div className="header-left d-flex align-items-center">
-          
-            <div className="button-toggle-menu">
-              <button className="header-btn" onClick={handleLeftMenuCallBack}>
-                <i className="fi fi-rr-menu-burger"></i>
-              </button>
-            </div>
-            <button
-              className={`navbar-toggle ${navOpen ? 'open' : ''}`}
-              data-bs-toggle="collapse"
-              data-bs-target="#topnav-menu-content"
-              onClick={toggleMenu}
-            >
-              <div className="lines">
-                <span />
-                <span />
-                <span />
-              </div>
-            </button>
-            <MegaMenu />
-          </div>
-          {/* header-right */}
-          <div className="header-right d-flex align-items-center justify-content-center">
-            <Search />
-            <DarkLight />
-            <Languages />
-            <span className="d-none d-sm-flex">
-              <Activity />
-              <Helpdesk />
-              <Applications />
-            </span>
-            <Notifications />
-            {user?.currDate && (
+          {user?.currDate && (
               <div className="header-btn px-2">
                 <span className="text-muted small">Business Date: {user.currDate}</span>
               </div>
             )}
+           
+          </div>
+          {/* header-right */}
+          <div className="header-right d-flex align-items-center justify-content-center">
+            
+            <DarkLight />
+            <Languages />
+            <span className="d-none d-sm-flex">
+              
+              <Helpdesk />
+              
+            </span>
+            <Notifications />
+            
             <Profile />
             <div className="header-btn pe-md-0 d-lg-none" onClick={handleMegaMenuClick}>
               <i className="fi fi-rr-menu-burger"></i>
