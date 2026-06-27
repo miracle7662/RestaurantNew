ecf9e8fe19aee6d0512f6dc89274bd4a72a7c389
Sharmintamboli1 <110376358+Sharmintamboli1@users.noreply.github.com>
2026-06-27 22:20:49 +0530

    updates

--- backend/modules/hotelbooking/routes/DiscountRoutes.js
+++ backend/modules/hotelbooking/routes/DiscountRoutes.js
@@ -6,16 +6,10 @@ const DiscountController = require('../controllers/DiscountController')
 // POST - Apply discount
 router.post('/apply', DiscountController.applyDiscount)
 
-// GET - Get discount details (with filters)
 
-// GET - Get discount summary
 
 // GET - Get ALL days for a room
 router.get('/room/:roomId', DiscountController.getRoomDiscountDetails)
 
-// GET - Get discount by ID
-
-// DELETE - Remove discount
-router.delete('/:detailId', DiscountController.removeDiscount)
 
 module.exports = router
